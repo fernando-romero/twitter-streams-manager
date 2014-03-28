a7ec45577b803cca7c9931e6645ee07476e4af62
Fernando Romero <fernando.romero@rightscale.com>
2014-03-28 11:48:06 -0500

    removing unnecessary dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -15,18 +15,6 @@ object ApplicationBuild extends Build {
     "org.elasticsearch" % "elasticsearch" % "1.0.1"
   )
 
-  val dataProcessorDependencies = Seq(
-    "com.typesafe.play" %% "play-cache" % "2.2.2",
-    "com.typesafe" %% "play-plugins-redis" % "2.1.1",
-    "com.typesafe" %% "play-plugins-mailer" % "2.2.0",
-    "com.tzavellas" % "sse-breaker" % "0.8.0",
-    "com.amazonaws" % "aws-java-sdk" % "1.3.27",
-    "org.elasticsearch" % "elasticsearch" % "0.20.2",
-    "com.spatial4j" % "spatial4j" % "0.3" ,
-    "commons-io" % "commons-io" % "2.4",
-    "org.apache.poi" % "poi" % "3.9"
-  )
-
   val main = play.Project(appName, appVersion, appDependencies)
     .settings(
       resolvers += Resolver.url("Objectify Play Repository", url("http://schaloner.github.com/releases/"))(Resolver.ivyStylePatterns),
